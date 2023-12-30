2909e2959b8b90b1e3556dd3bda1ad86c5bf0323
AlekLimaTeles <152651216+AlekLimaTeles@users.noreply.github.com>
2023-12-30 09:02:57 -0300

    Environment Variables
    Command Line:
    "npm i dotenv"

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import crypto from 'node:crypto'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-
-  return tables
+  const users = await knex('users').select('*')
+    
+  return users
 })
 
 app
